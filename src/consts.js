454c0ef605fec2806abb938a6de346a553d43a4a
niehaojie <113283397+niehaojie@users.noreply.github.com>
2023-06-15 00:24:14 +0800

    Initial commit

---
+++ src/consts.js
@@ -0,0 +1,5 @@
+export const SITE_TITLE = `Xi Blog`;
+export const SITE_DESCRIPTION = 'Site Description';
+export const SITE_EMAIL = '9@xihaonie.com'
+export const SITE_NAME = 'chimelong.github.io';
+export const SITE_URL = "https://chimelong.github.io";
