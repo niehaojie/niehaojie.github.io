0cf9b734a9563339da3031c2ea97a89ec277d16e
niehaojie <113283397+niehaojie@users.noreply.github.com>
2023-06-16 00:54:52 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Nie Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = '9@xihaonie.com'
+export const SITE_EMAIL = 'haojienie@icloud.com'
 export const SITE_NAME = 'chimelong.github.io';
 export const SITE_URL = "https://chimelong.github.io";
